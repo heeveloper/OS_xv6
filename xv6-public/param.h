2894723a106da502a403af94bbd1de98b1858bf5
heejun <zaqq1414@hanyang.ac.kr>
2018-05-30 03:11:25 +0900

    set FSSIZE 40000

--- xv6-public/param.h
+++ xv6-public/param.h
@@ -10,7 +10,7 @@
 #define MAXOPBLOCKS  10  // max # of blocks any FS op writes
 #define LOGSIZE      (MAXOPBLOCKS*3)  // max data blocks in on-disk log
 #define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache
-#define FSSIZE       1000  // size of file system in blocks
+#define FSSIZE       40000  // size of file system in blocks
 #define TRUE          1
 #define FALSE         0
 #define TOTALTICKET  10000 // the number of total tickets used in Stride scheduler
